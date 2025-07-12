db176c4736829b65a256420a50458980206bea58
harshil0217 <harshil0217@gmail.com>
2025-07-11 21:37:20 -0400

    rewrote for langraph

--- agent_script.py
+++ agent_script.py
@@ -1,10 +1,19 @@
 #import libraries
+from typing import List
+from typing import Annotated
+from typing_extensions import TypedDict
 from mcp import ClientSession, StdioServerParameters
 from mcp.client.stdio import stdio_client
 from langchain_mcp_adapters.tools import load_mcp_tools 
 from langchain_mcp_adapters.client import MultiServerMCPClient
+from langchain_mcp_adapters.prompts import load_mcp_prompt
+from langchain.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langgraph.prebuilt import create_react_agent
 from langchain_openai import ChatOpenAI
+from langgraph.graph.message import AnyMessage, add_messages
+from langgraph.graph import StateGraph, START, END
+from langgraph.checkpoint.memory import MemorySaver
+from langgraph.prebuilt import tools_condition, ToolNode
 import asyncio
 from asyncio.exceptions import TimeoutError
 import os
@@ -55,7 +64,7 @@ client = MultiServerMCPClient(
             "env": {
                 "FIRECRAWL_API_KEY": firecrawl_key
             },
-            "transport": "stdio"
+            "transport": "streamable-http"
             
         },
         "spotify": {
@@ -70,11 +79,52 @@ client = MultiServerMCPClient(
                 "SPOTIFY_CLIENT_SECRET": os.getenv("SPOTIFY_CLIENT_SECRET"),
                 "SPOTIFY_REDIRECT_URI": os.getenv("SPOTIFY_REDIRECT_URI")
             },
-            "transport": "stdio"
+            "transport": "streamable-http"
                    
         }
     }     
 )
+
+async def create_graph(firecrawl_session, spotify_session):
+    llm = ChatOpenAI(model="gpt-4o")
+    
+    firecrawl_tools = await load_mcp_tools(firecrawl_session)
+    spotify_tools = await load_mcp_tools(spotify_session)
+    
+    tools = firecrawl_tools + spotify_tools
+    llm_with_tools = llm.bind_tools(tools)
+    
+    firecrawl_system_prompt = await load_mcp_prompt(server_name="firecrawl", prompt_name="system_prompt")
+    spotify_system_prompt = await load_mcp_prompt(server_name="spotify", prompt_name="system_prompt")
+    system_prompt = firecrawl_system_prompt + "\n\n" + spotify_system_prompt
+    
+    prompt_template = ChatPromptTemplate.from_messages([
+        ("system", system_prompt[0].content),
+        MessagesPlaceholder("messages")
+    ])
+    
+    chat_llm = prompt_template | llm_with_tools
+    
+    # State Management
+    class State(TypedDict):
+        messages: Annotated[List[AnyMessage], add_messages]
+        
+    def chat_node(state: State) -> State:
+        state["messages"] = chat_llm.invoke({"messages": state["messages"]})
+        return state
+
+    # Building the graph
+    graph_builder = StateGraph(State)
+    graph_builder.add_node("chat_node", chat_node)
+    graph_builder.add_node("tool_node", ToolNode(tools=tools))
+    graph_builder.add_edge(START, "chat_node")
+    graph_builder.add_conditional_edges("chat_node", tools_condition, {"tools": "tool_node", "__end__": END})
+    graph_builder.add_edge("tool_node", "chat_node")
+    graph = graph_builder.compile(checkpointer=MemorySaver())
+    return graph
+
+
+'''
 warnings.filterwarnings("ignore", category=ResourceWarning)
     
 async def run_agent():
@@ -147,3 +197,4 @@ external API calls it makes (e.g., Brave Search).
         response = f"An error occurred: {e}"
 
     pprint(response)
+'''
